1e49fbf303fb66608815140debbe5a7662c82a52
joacomendiola <joakomendiola25@gmail.com>
2025-08-30 17:41:42 -0300

    Division de carpetas

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,4 @@
-package TP1;
+package app;
 
 public class Main {
     public static void main(String[] args) {
--- src/service/Pyme.java
+++ src/service/Pyme.java
@@ -1,4 +1,4 @@
-package TP1;
+package service;
 
 import java.util.ArrayList;
 import java.util.Collections;
